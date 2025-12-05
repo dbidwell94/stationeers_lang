c798f98407a0917d9bbf42bf4a68629ea04ea737
Devin Bidwell <dbidwell94@biddydev.com>
2025-12-05 03:35:35 -0700

    Add 1 more dll to the build artifacts

--- build.sh
+++ build.sh
@@ -37,6 +37,7 @@ echo "Copying Release files to output directory"
 echo "--------------------"
 
 RUST_WIN_EXE="$RUST_DIR/target/x86_64-pc-windows-gnu/release/slang.exe"
+RUST_WIN_DLL="$RUST_DIR/target/x86_64-pc-windows-gnu/release/slang.dll"
 RUST_LINUX_BIN="$RUST_DIR/target/x86_64-unknown-linux-gnu/release/slang"
 CHARP_DLL="$CSHARP_DIR/bin/Release/net48/StationeersSlang.dll"
 
@@ -45,6 +46,11 @@ if [[ ! -d "$RELEASE_DIR" ]]; then
   mkdir "$RELEASE_DIR"
 fi
 
+# This is the windows binary
 cp "$RUST_WIN_EXE" "$RELEASE_DIR/slang.exe"
+# This is the linux executable
 cp "$RUST_LINUX_BIN" "$RELEASE_DIR/slang"
+# This is the DLL mod itself
 cp "$CHARP_DLL" "$RELEASE_DIR/StationeersSlang.dll"
+# This is the rust-only compiler for use in injecting into the mod
+cp "$RUST_WIN_DLL" "$RELEASE_DIR/rust_slang.dll"
